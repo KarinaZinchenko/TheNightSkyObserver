b82373070e1565ad647542091a905f3d273d6a2b
marcosson <luca.bassi.89@gmail.com>
2017-04-23 11:29:41 +0200

    Form resa consistente con inserimento
    Form di modifica resa consistente con form di inserimento: non vengono mostrati tutti i campi in caso di modifica binocolo e le fomule vengono calcolate automaticamente.

--- modificaEliminaStrumento.php
+++ modificaEliminaStrumento.php
@@ -20,28 +20,103 @@ if (isset($_SESSION["autenticato"]) && isset($_SESSION["tipo"])) {
             ?>
         <html>
         <head>
+          <title>Modifica/Elimina strumento</title>
+          <script type="text/javascript" language="javascript">
+            function calcolaFocalRatio() {
+              lunghezzaFocale = document.getElementsByName("lunghezzaFocale")[0].value;
+              aperturaMillimetri = document.getElementsByName("aperturaMillimetri")[0].value;
+              campoFocalRatio = document.getElementsByName("focalRatio")[0];
+              if (aperturaMillimetri != 0) {
+                focalRatio = lunghezzaFocale/aperturaMillimetri;
+                campoFocalRatio.value = focalRatio;
+              } else {
+                campoFocalRatio.value = "Apertura deve essere diverso da zero!";
+              }
+            }
+
+            function calcolaStimaPotereRisolutivoI() {
+              aperturaMillimetri = document.getElementsByName("aperturaMillimetri")[0].value;
+              campoStimaPotereRisolutivo = document.getElementsByName("stimaPotereRisolutivo")[0];
+              campoStimaPotereRisolutivo.value = 3.7 + 2.5 * Math.log(Math.pow(aperturaMillimetri, 2));
+            }
+
+            function calcolaStimaPotereRisolutivoII() {
+              aperturaPollici = document.getElementsByName("aperturaPollici")[0].value;
+              campoStimaPotereRisolutivo = document.getElementsByName("stimaPotereRisolutivo")[0];
+              campoStimaPotereRisolutivo.value = 9.5 + 5 * Math.log(Math.pow(aperturaPollici, 2));
+            }
+
+            function calcolaRisoluzioneAngolare() {
+              aperturaPollici = document.getElementsByName("aperturaPollici")[0].value;
+              campoRisoluzioneAngolare = document.getElementsByName("risoluzioneAngolare")[0];
+              campoRisoluzioneAngolare.value = 4.56/aperturaPollici;
+            }
+          </script>
         </head>
         <body>
           <center>
             <form method="post" action="modificaEliminaStrumentoEffettivo.php">
               <input type="hidden" name="IDStrumento" value="<?php echo $_POST["IDStrumento"];  ?>" />
-              Nome: <input type="text" name="nome" value="<?php echo $tupla['nome']; ?>"><br><br>
-              Note: <input type="text" name="note" value="<?php echo $tupla['note']; ?>"><br><br>
-              Tipo: <input type="text" name="tipo" value="<?php echo $tupla['tipo'];?>"><br><br>
-              Marca: <input type="text" name="marca" value="<?php echo $tupla['marca'];?>"><br><br>
-              Disponibilit&agrave;: <input type="text" name="disponibilita" value="<?php echo $tupla['disponibilita'];?>"><br><br>
-              Apertura: <input type="text" name="apertura" value="<?php echo $tupla['apertura'];?>"><br><br>
-              Campo focale: <input type="text" name="campoFocale" value="<?php echo $tupla['campo_focale'];?>"><br><br>
-              ingrandimenti: <input type="date" name="ingrandimenti" value="<?php echo $tupla['ingrandimenti'];?>"><br><br>
-              Lunghezza focale: <input type="date" name="lunghezzaFocale" value="<?php echo $tupla['lunghezza_focale'];?>"><br><br>
-              Montatura: <input type="date" name="montatura" value="<?php echo $tupla['montatura'];?>"><br><br>
-              Focal ratio: <input type="date" name="focalRatio" value="<?php echo $tupla['focal_ratio'];?>"><br><br>
-              Campo cercatore: <input type="date" name="campoCercatore" value="<?php echo $tupla['campo_cercatore'];?>"><br><br>
-              Tipo telescopio: <input type="date" name="tipoTelescopio" value="<?php echo $tupla['tipo_telescopio'];?>"><br><br>
-              Apertura millimetri: <input type="date" name="aperturaMillimetri" value="<?php echo $tupla['apertura_millimetri'];?>"><br><br>
-              Apertura pollici: <input type="date" name="aperturaPollici" value="<?php echo $tupla['apertura_pollici'];?>"><br><br>
-              Risoluzione angolare: <input type="date" name="risoluzioneAngolare" value="<?php echo $tupla['risoluzione_angolare'];?>"><br><br>
-              Stima potere risolutivo: <input type="date" name="stimaPotereRisolutivo" value="<?php echo $tupla['stima_potere_risolutivo'];?>"><br><br>
+              <p>Nome: <input type="text" name="nome" value="<?php echo $tupla['nome']; ?>"></p>
+              <p>Note: <input type="text" name="note" value="<?php echo $tupla['note']; ?>"></p>
+              <p>Tipo: <input type="text" name="tipo" value="<?php echo $tupla['tipo'];?>"></p>
+              <p>Marca: <input type="text" name="marca" value="<?php echo $tupla['marca'];?>"></p>
+              <p>Disponibilit&agrave;: <input type="text" name="disponibilita" value="<?php echo $tupla['disponibilita'];?>"></p>
+              <p>Apertura: <input type="text" name="apertura" value="<?php echo $tupla['apertura'];?>"></p>
+                <?php
+                if ($tupla['tipo'] == "Telescopio") {
+                    $campoFocale = $tupla['campo_focale'];
+                    echo "<p class=\"telescopio\">Campo focale: <input type=\"text\" name=\"campoFocale\" value=\"$campoFocale\"></p>";
+                }
+                ?>
+              <p>Ingrandimenti: <input type="date" name="ingrandimenti" value="<?php echo $tupla['ingrandimenti'];?>"></p>
+                <?php
+                if ($tupla['tipo'] == "Telescopio") {
+                    $lunghezzaFocale = $tupla['lunghezza_focale'];
+                    $montatura = $tupla['montatura'];
+                    $campoCercatore = $tupla['campo_cercatore'];
+                    $tipoTelescopio = $tupla['tipo_telescopio'];
+                    echo "<p class=\"telescopio\">Lunghezza focale: <input type=\"text\" name=\"lunghezzaFocale\" oninput=\"calcolaFocalRatio()\" value=\"$lunghezzaFocale\"></p>";
+                    echo "<p class=\"telescopio\">Montatura: <input type=\"text\" name=\"montatura\" value=\"$montatura\"></p>";
+                    echo "<p class=\"telescopio\">Campo del cercatore: <input type=\"text\" name=\"campoCercatore\" value=\"$campoCercatore\"></p>";
+                    echo "<p class=\"telescopio\">Tipo di telescopio: <select name=\"tipoTelescopio\" value=\"$tipoTelescopio\"></p>";
+                    echo "<option value=\"Rifrattore\"";
+                    if ($tipoTelescopio == "Rifrattore") {
+                        echo "selected = \"selected\"";
+                    }
+                    echo ">Rifrattore</option>";
+                    echo "<option value=\"Riflettore\"";
+                    if ($tipoTelescopio == "Riflettore") {
+                        echo "selected = \"selected\"";
+                    }
+                    echo ">Riflettore</option>";
+                    echo "<option value=\"Catadiottrico\"";
+                    if ($tipoTelescopio == "Catadiottrico") {
+                        echo "selected = \"selected\"";
+                    }
+                    echo ">Catadiottrico</option>";
+                    echo "<option value=\"Altro\"";
+                    if ($tipoTelescopio == "Altro") {
+                        echo "selected = \"selected\"";
+                    }
+                    echo ">Altro</option>";
+                    echo "</select>";
+                }
+                ?>
+              <p>Apertura (in millimetri): <input type="date" name="aperturaMillimetri" oninput="calcolaFocalRatio(); calcolaStimaPotereRisolutivoI()" value="<?php echo $tupla['apertura_millimetri'];?>">
+              </p>
+                <?php
+                if ($tupla['tipo'] == "Telescopio") {
+                    $aperturaPollici = $tupla['apertura_pollici'];
+                    $focalRatio = $tupla['focal_ratio'];
+                    $risoluzioneAngolare = $tupla['risoluzione_angolare'];
+                    $stimaPotereRisolutivo = $tupla['stima_potere_risolutivo'];
+                    echo "<p class=\"telescopio\">Apertura (in pollici): <input type=\"text\" name=\"aperturaPollici\" value=\"$aperturaPollici\" oninput=\"calcolaRisoluzioneAngolare()\"></p>";
+                    echo "<p class=\"telescopio\">Focal Ratio: <input type=\"text\" name=\"focalRatio\" value=\"$focalRatio\" readonly></p>";
+                    echo "<p class=\"telescopio\">Risoluzione angolare: <input type=\"text\" name=\"risoluzioneAngolare\" value=\"$risoluzioneAngolare\" readonly></p>";
+                    echo "<p class=\"telescopio\">Stima potere risolutivo: <input type=\"text\" name=\"stimaPotereRisolutivo\" value=\"$stimaPotereRisolutivo\" readonly></p>";
+                }
+                ?>
               <select name="scelta_operazione">
                 <option value="modifica">Modifica</option>
                 <option value="elimina">Elimina</option>
