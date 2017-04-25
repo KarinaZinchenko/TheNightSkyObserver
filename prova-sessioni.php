8aff2e733648d4c313547e68e1e23fc1ee8a16b7
Karina Zinchenko <karina.19@hotmail.it>
2017-04-25 16:07:41 +0200

    Update prova-sessioni.php

--- prova-sessioni.php
+++ prova-sessioni.php
@@ -12,23 +12,33 @@ if(isset($_SESSION["autenticato"]))
 	{
          if($tipo=="regolare")
          {
-         	echo "<a href='inserisciOsservazione.php'>Crea osservazione </a>"; 
-            echo "&nbsp; &nbsp; <a href='modificaEliminaOsservazione.php'>Modifica/Elimina Osservazione </a>";// pagina personale utente regolare
-         }
+		echo "<a href='inserisciOsservazione.php'>Crea osservazione </a>"; 
+		echo "&nbsp; &nbsp; <a href='modificaEliminaOsservazione.php'>Modifica/Elimina Osservazione </a>";// pagina personale utente regolare
+		echo "&nbsp; &nbsp; <a href='vistaSoci.php'>Vista Soci</a>";
+		echo "&nbsp; &nbsp; <a href='vistaOsservazioniNonComplete.php'>Vista Osservazioni Non Complete</a>";
+		echo "&nbsp; &nbsp; <a href='vistaOggettiCelesti.php'>Vista Oggetti Celesti</a>";
+		echo "&nbsp; &nbsp; <a href='vistaSitiOsservativi.php'>Vista Siti Osservativi</a>";
+	 }
          elseif ($tipo=="amministratore") 
          {
-            echo "utente amministratore <br>";
-            echo "<a href='inserisciUtente.php'>Inserisci nuovo utente </a>";
-            echo "&nbsp; &nbsp; <a href='modificaEliminaUtente.php'>Modifica/Elimina Utenti </a>";
-            echo "&nbsp; &nbsp; <a href='inserisciOculare.php'>Inserisci Oculare </a>";
-            echo "&nbsp; &nbsp; <a href='inserisciFiltro.php'>Inserisci Filtro/Altro </a>";
-            echo "&nbsp; &nbsp; <a href='inserisciOggettoCeleste.php'>Inserisci Oggetto Celeste </a>";
-	    echo "&nbsp; &nbsp; <a href='inserisciArea.php'>Inserisci Sito Osservativo </a>";
-	    echo "&nbsp; &nbsp; <a href='inserisciStrumento.php'>Inserisci Strumento </a>";
-	    echo "&nbsp; &nbsp; <a href='modificaEliminaStrumento.php'>Modifica/Elimina Strumento</a>";
-        echo "<br><br>";
-        echo "<a href='inserisciOsservazione.php'>Crea osservazione </a>";
-        echo "&nbsp; &nbsp; <a href='modificaEliminaOsservazione.php'>Modifica/Elimina Osservazione </a>";
+		echo "utente amministratore <br>";
+		echo "<a href='inserisciUtente.php'>Inserisci nuovo utente </a>";
+		echo "&nbsp; &nbsp; <a href='modificaEliminaUtente.php'>Modifica/Elimina Utenti </a>";
+		echo "&nbsp; &nbsp; <a href='inserisciOculare.php'>Inserisci Oculare </a>";
+		echo "&nbsp; &nbsp; <a href='inserisciFiltro.php'>Inserisci Filtro/Altro </a>";
+		echo "&nbsp; &nbsp; <a href='inserisciOggettoCeleste.php'>Inserisci Oggetto Celeste </a>";
+		echo "&nbsp; &nbsp; <a href='inserisciArea.php'>Inserisci Sito Osservativo </a>";
+		echo "&nbsp; &nbsp; <a href='inserisciStrumento.php'>Inserisci Strumento </a>";
+		echo "&nbsp; &nbsp; <a href='modificaEliminaStrumento.php'>Modifica/Elimina Strumento</a>";
+	    
+		echo "<br><br>";
+		echo "<a href='inserisciOsservazione.php'>Crea osservazione </a>";
+		echo "&nbsp; &nbsp; <a href='modificaEliminaOsservazione.php'>Modifica/Elimina Osservazione </a>";
+		echo "&nbsp; &nbsp; <a href='vistaSociInsolventi.php'>Vista Soci Insolventi</a>";
+		echo "&nbsp; &nbsp; <a href='vistaSoci.php'>Vista Soci</a>";
+		echo "&nbsp; &nbsp; <a href='vistaOsservazioniNonComplete.php'>Vista Osservazioni Non Complete</a>";
+		echo "&nbsp; &nbsp; <a href='vistaOggettiCelesti.php'>Vista Oggetti Celesti</a>";
+		echo "&nbsp; &nbsp; <a href='vistaSitiOsservativi.php'>Vista Siti Osservativi</a>";
         }
          else
            echo "utente insolvente";	
