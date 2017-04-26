8e10e128acae75290cbcf4849a02b9c7af0ea08d
andreamazzocut <andreamazzocut@gmail.com>
2017-04-26 17:45:07 +0200

    Add files via upload

--- inserisciOsservazione.php
+++ inserisciOsservazione.php
@@ -1,36 +1,65 @@
+ <html>
+         <head>
+          <title>Inserimento nuova osservazione</title>
+           
+         </head>
+         <body>
 <?php
 session_start();
 include("config.php");
+
 if(isset($_SESSION["autenticato"])&& isset($_SESSION["tipo"]))
 {
 	if($_SESSION["tipo"]=="regolare" || $_SESSION["tipo"]=="amministratore")
-	{
+	{ 
+    if(isset($_POST["invio_numero_oggetti_osservati"]))
+    {
+         $numero_osservazione=$_POST["numero_osservazioni"];
        ?>
-        <html>
-         <head>
-         	<title>Inserimento nuova osservazione</title>
-         </head>
-         <body>
-         <center>
+         <center> 
           <h1> Inserisci i dati dell'osservazione: </h1>  <br><br>
          <form method="post" action="inserisciOsservazioneEffettivo.php">
-         stato: &nbsp; <select name="stato" >
-        <option value="non_completata" selected="selected"> non completata </option>
-        <option value="completata"> completata </option>
-         </select><br><br>
+        
          categoria: <input type="text" name="categoria"><br><br>
         Trasparenza: <input type="number" name="trasparenza" step=any><br><br>
         Seeing (scala antoniani): <input type="number" name="seeing_antoniani" step=any><br><br>
         Seeing (scala pickering): <input type="number" name="seeing_pickering" step=any><br><br>
+        Area osservazione: &nbsp; <select name="area_osservazione">
+        <?php 
+           $sql="SELECT ID,nome FROM areageografica;";
+            $risposta=mysqli_query($conn,$sql)or die("Errore nella query: " . $sql . "\n" . mysqli_error($conn));
+            if(mysqli_num_rows($risposta)!=0)
+                 {
+                   while ($tupla = mysqli_fetch_array($risposta))
+                    {
+                              $id=$tupla["ID"];
+                              $nome=$tupla["nome"];
+                              ?>
+                              <option value="<?php echo "$id";?>"><?php echo "$id - $nome ";?> </option>
+                              <?php
+                    }
+                 }
+
+        ?>
+
+       </select> <br><br>
+        <?php
+        for($i=1;$i<=$numero_osservazione;$i++)
+          { echo "<hr>";
+        ?>
+         stato: &nbsp; <select name="stato<?php echo $i; ?>" >
+        <option value="pianificata" selected="selected"> pianificata </option>
+        <option value="conclusa"> conclusa </option>
+         </select><br><br>
      <?php //fino a qui relativo a tabella osservazione  ?>
 
-        rating: <input type="number" name="rating" step=any><br><br>
-        descrizione: <input type="text" name="descrizione"><br><br>
-        immagine: <input type="text" name="immagine"><br><br>
-         note: <input type="text" name="note"><br><br>
-        ora  inizio: <input type="text" name="ora_inizio"><br><br>
-        ora fine: <input type="text" name="ora_fine"><br><br>
-       Oggetto celeste osservato: &nbsp; <select name="oggetto_celeste">
+        rating: <input type="number" name="rating<?php echo $i ?>" step=any><br><br>
+        descrizione: <input type="text" name="descrizione<?php echo $i ?>"><br><br>
+        immagine: <input type="text" name="immagine<?php echo $i ?>"><br><br>
+         note: <input type="text" name="note<?php echo $i ?>"><br><br>
+        ora  inizio: <input type="text" name="ora_inizio<?php echo $i ?>"><br><br>
+        ora fine: <input type="text" name="ora_fine<?php echo $i ?>"><br><br>
+       Oggetto celeste osservato: &nbsp; <select name="oggetto_celeste<?php echo $i ?>">
        <?php 
            $sql="SELECT ID,nome FROM oggettoceleste;";
             $risposta=mysqli_query($conn,$sql)or die("Errore nella query: " . $sql . "\n" . mysqli_error($conn));
@@ -48,9 +77,9 @@ if(isset($_SESSION["autenticato"])&& isset($_SESSION["tipo"]))
 
         ?>
 
-       </select> <br><br>
+       </select>  <br><br>
 
-      Oculare Usato: &nbsp; <select name="oculare">
+      Oculare Usato: &nbsp; <select name="oculare<?php echo $i ?>">
        <?php 
            $sql="SELECT ID,nome FROM oculare;";
             $risposta=mysqli_query($conn,$sql)or die("Errore nella query: " . $sql . "\n" . mysqli_error($conn));
@@ -70,7 +99,7 @@ if(isset($_SESSION["autenticato"])&& isset($_SESSION["tipo"]))
 
        </select> <br><br>
 
-         Filtro usato: &nbsp; <select name="filtro">
+         Filtro usato: &nbsp; <select name="filtro<?php echo $i ?>">
        <?php 
            $sql="SELECT ID,nome FROM filtro_altro;";
             $risposta=mysqli_query($conn,$sql)or die("Errore nella query: " . $sql . "\n" . mysqli_error($conn));
@@ -90,7 +119,7 @@ if(isset($_SESSION["autenticato"])&& isset($_SESSION["tipo"]))
 
        </select> <br><br>
 
-        Strumento Usato: &nbsp; <select name="strumento">
+        Strumento Usato: &nbsp; <select name="strumento<?php echo $i ?>">
        <?php 
            $sql="SELECT ID,nome FROM strumento;";
             $risposta=mysqli_query($conn,$sql)or die("Errore nella query: " . $sql . "\n" . mysqli_error($conn));
@@ -109,34 +138,32 @@ if(isset($_SESSION["autenticato"])&& isset($_SESSION["tipo"]))
         ?>
 
        </select> <br><br>
-
-        Area osservazione: &nbsp; <select name="area_osservazione">
+     
         <?php 
-           $sql="SELECT ID,nome FROM areageografica;";
-            $risposta=mysqli_query($conn,$sql)or die("Errore nella query: " . $sql . "\n" . mysqli_error($conn));
-            if(mysqli_num_rows($risposta)!=0)
-                 {
-                 	 while ($tupla = mysqli_fetch_array($risposta))
-    	              {
-                              $id=$tupla["ID"];
-                              $nome=$tupla["nome"];
-                              ?>
-                              <option value="<?php echo "$id";?>"><?php echo "$id - $nome ";?> </option>
-                              <?php
-    	              }
-                 }
-
-        ?>
-
-       </select> <br><br>
+         }// chiudo il for
+         $_SESSION["numero_osservazione"]=$numero_osservazione;
+      ?>
          <input type="hidden" name="id_utente" value="<?php echo $_SESSION["numero_socio"];  ?>" />
+        
 	     <input type="submit" name="invio">
          </form>
+         
          </center>
          </body>
          </html>
 
        <?php
+     }
+     else
+     {
+         ?> <form method="post" action="<?php echo $_SERVER['PHP_SELF'] ?>" >
+            inserire il numero di osservazioni che si intende fare: &nbsp; <input type="number" name="numero_osservazioni"/>&nbsp;
+            <input type="submit" name="invio_numero_oggetti_osservati" value="scegli" />
+            </form>
+            <?php
+     } //fine controllo se devo inserire numero di oss!
+
+
      }
       else
     {
--- inserisciOsservazioneEffettivo.php
+++ inserisciOsservazioneEffettivo.php
@@ -5,13 +5,52 @@ if(isset($_SESSION["autenticato"])&& isset($_SESSION["tipo"]))
 {
 	if($_SESSION["tipo"]=="amministratore" || $_SESSION["tipo"]=="regolare")
 	{
+   
         
         $categoria="";
         $trasparenza=null;
-        $seeing_antoniani=null;
-        $seeing_pickering=null;
-        $stato="";
-        $rating=null;
+        $seeing_antoniani=-1;
+        $seeing_pickering=-1;
+        $id_area_geografica=null;
+        $id_anagrafica=null;
+        $id_osservazioni=null;
+       
+        
+       
+  
+        if($_SERVER["REQUEST_METHOD"]=="POST")
+       {
+         $numero_osservazione=$_SESSION["numero_osservazione"];
+        if($_REQUEST["categoria"]!="" && $_REQUEST["trasparenza"]!="")
+        {
+        $categoria=$_REQUEST["categoria"];
+        $trasparenza=$_REQUEST["trasparenza"];
+        $seeing_antoniani=$_REQUEST["seeing_antoniani"];
+        if($seeing_antoniani==null){$seeing_antoniani=-1;}
+        $seeing_pickering=$_REQUEST["seeing_pickering"];
+        if($seeing_pickering==null){$seeing_pickering=-1;}
+        $id_area_geografica=$_REQUEST["area_osservazione"];
+        $id_anagrafica=$_REQUEST["id_utente"];
+        $categoria=nl2br(htmlentities($categoria,ENT_QUOTES,'UTF-8'));
+           $trasparenza=nl2br(htmlentities($trasparenza,ENT_QUOTES,'UTF-8'));
+            $seeing_antoniani=nl2br(htmlentities($seeing_antoniani,ENT_QUOTES,'UTF-8'));
+             $seeing_pickering=nl2br(htmlentities($seeing_pickering,ENT_QUOTES,'UTF-8'));
+             $id_area_geografica=nl2br(htmlentities($id_area_geografica,ENT_QUOTES,'UTF-8'));
+                          $id_anagrafica=nl2br(htmlentities($id_anagrafica,ENT_QUOTES,'UTF-8'));
+        }
+        else
+        {
+          echo "<script language='javascript'>"; 
+         echo "alert(' i campi obbligatori devono essere inseriti');";
+        echo " </script>";
+         echo "<a href='inserisciOsservazione.php'>Torna alla pagina per inserire l'osservazione </a> ";
+        }
+            $sql="INSERT INTO osservazioni(categoria,trasparenza,seeing_antoniani,seeing_pickering,id_area_geografica,id_anagrafica) VALUES ('".addslashes($categoria)."',".addslashes($trasparenza).",".addslashes($seeing_antoniani).",".addslashes($seeing_pickering).",".addslashes($id_area_geografica).",".addslashes($id_anagrafica).");";
+            mysqli_query($conn,$sql)or die("Errore nella query: " . $sql . "\n" . mysqli_error($conn));
+
+        for($i=1;$i<=$numero_osservazione;$i++)
+          {$stato="";
+        $rating=-1;
         $descrizione="";
         $immagini="";
         $note="";
@@ -19,37 +58,26 @@ if(isset($_SESSION["autenticato"])&& isset($_SESSION["tipo"]))
         $ora_fine="";
         $id_oggettoceleste=null;
         $id_strumento=null;
-        $id_osservazioni=null;
         $id_oculare=null;
         $id_filtro=null;
-        $id_area_geografica=null;
-        $id_anagrafica=null;
-        if($_SERVER["REQUEST_METHOD"]=="POST")
-       {
-       	if($_REQUEST["categoria"]!="" && $_REQUEST["trasparenza"]!="" && $_REQUEST["stato"]!="")
+       	if($_REQUEST["stato".$i]!="")
        	 {
-           $categoria=$_REQUEST["categoria"];
-        $trasparenza=$_REQUEST["trasparenza"];
-        $seeing_antoniani=$_REQUEST["seeing_antoniani"];
-        $seeing_pickering=$_REQUEST["seeing_pickering"];
-        $stato=$_REQUEST["stato"];
-        $rating=$_REQUEST["rating"];
-        $descrizione=$_REQUEST["descrizione"];
-        $immagini=$_REQUEST["immagine"];
-        $note=$_REQUEST["note"];
-        $ora_inizio=$_REQUEST["ora_inizio"];
-        $ora_fine=$_REQUEST["ora_fine"];
-        $id_oggettoceleste=$_REQUEST["oggetto_celeste"];
-        $id_strumento=$_REQUEST["strumento"];
-        $id_oculare=$_REQUEST["oculare"];
-        $id_filtro=$_REQUEST["filtro"];
-        $id_area_geografica=$_REQUEST["area_osservazione"];
-        $id_anagrafica=$_REQUEST["id_utente"];
+           
+        $stato=$_REQUEST["stato".$i];
+        $rating=$_REQUEST["rating".$i];
+        if($rating==null){$rating=-1;}
+        $descrizione=$_REQUEST["descrizione".$i];
+        $immagini=$_REQUEST["immagine".$i];
+        $note=$_REQUEST["note".$i];
+        $ora_inizio=$_REQUEST["ora_inizio".$i];
+        $ora_fine=$_REQUEST["ora_fine".$i];
+        $id_oggettoceleste=$_REQUEST["oggetto_celeste".$i];
+        $id_strumento=$_REQUEST["strumento".$i];
+        $id_oculare=$_REQUEST["oculare".$i];
+        $id_filtro=$_REQUEST["filtro".$i];
+        
 
-          $categoria=nl2br(htmlentities($categoria,ENT_QUOTES,'UTF-8'));
-           $trasparenza=nl2br(htmlentities($trasparenza,ENT_QUOTES,'UTF-8'));
-            $seeing_antoniani=nl2br(htmlentities($seeing_antoniani,ENT_QUOTES,'UTF-8'));
-             $seeing_pickering=nl2br(htmlentities($seeing_pickering,ENT_QUOTES,'UTF-8'));
+          
               $stato=nl2br(htmlentities($stato,ENT_QUOTES,'UTF-8'));
                $rating=nl2br(htmlentities($rating,ENT_QUOTES,'UTF-8'));
                 $descrizione=nl2br(htmlentities($descrizione,ENT_QUOTES,'UTF-8'));
@@ -61,56 +89,52 @@ if(isset($_SESSION["autenticato"])&& isset($_SESSION["tipo"]))
                       $id_strumento=nl2br(htmlentities($id_strumento,ENT_QUOTES,'UTF-8'));
                        $id_oculare=nl2br(htmlentities($id_oculare,ENT_QUOTES,'UTF-8'));
                         $id_filtro=nl2br(htmlentities($id_filtro,ENT_QUOTES,'UTF-8'));
-                         $id_area_geografica=nl2br(htmlentities($id_area_geografica,ENT_QUOTES,'UTF-8'));
-                          $id_anagrafica=nl2br(htmlentities($id_anagrafica,ENT_QUOTES,'UTF-8'));
+                         
 
-        $sql="INSERT INTO osservazioni(categoria,trasparenza,seeing_antoniani,seeing_pickering,id_area_geografica,id_anagrafica) VALUES ('".addslashes($categoria)."',".addslashes($trasparenza).",".addslashes($seeing_antoniani).",".addslashes($seeing_pickering).",".addslashes($id_area_geografica).",".addslashes($id_anagrafica).");";
+      
 
-         if(mysqli_query($conn, $sql) )
-       {
        	  $sql="SELECT MAX(ID) as ID from osservazioni";
-       	  $risposta=mysqli_query($conn,$sql)or die("Errore nella query: " . $sql . "\n" . mysqli_error($conn));
+       	  $risposta=mysqli_query($conn,$sql)or die("Errore nella query: " . $sql . " al passaggio ".$i."\n" . mysqli_error($conn));
             if(mysqli_num_rows($risposta)!=0)
                  {
                  	$tupla = mysqli_fetch_array($risposta);
                    $id_osservazioni=$tupla["ID"];
 
                    $sql="INSERT INTO datiosservazione(stato,rating,descrizione,immagine,note,ora_fine,ora_inizio,id_oggettoceleste,id_strumento,id_osservazioni,id_oculare,id_filtro) VALUES ('".addslashes($stato)."',".addslashes($rating).",'".addslashes($descrizione)."','".addslashes($immagini)."','".addslashes($note)."','".addslashes($ora_fine)."','".addslashes($ora_inizio)."',".addslashes($id_oggettoceleste).",".addslashes($id_strumento).",".addslashes($id_osservazioni).",".addslashes($id_oculare).",".addslashes($id_filtro).");";
-                   if(mysqli_query($conn, $sql) )
-       {
-            echo "<script language='javascript'>"; 
-       	    echo "alert('Inserimento effettuato');";
-             echo " </script>";
-             echo "<a href='prova-sessioni.php'>Torna alla pagina personale </a> ";
-       } 
-          else {die("Errore nella query: "     . $sql . "\n" . mysqli_error($conn));}
-
- 	 mysqli_close($conn);
+                   mysqli_query($conn, $sql) or die("Errore nella query: "     . $sql . "\n" . mysqli_error($conn));
+       
+            
 
                  }
                  else
                  {
                  	die("errore nelle query");
                  }
+               } // chiudo if per controllare se lo stato è stato inserito 
+                else
+                 {echo "<script language='javascript'>"; 
+                echo "alert(' i campi obbligatori devono essere inseriti');";
+                   echo " </script>";
+                     echo "<a href='inserisciOsservazione.php'>Torna alla pagina per inserire l'osservazione </a> ";
+                   }
 
-       } 
-          else {die("Errore nella query: "     . $sql . "\n" . mysqli_error($conn));}
-        
-       	 }
-       	 else
-       	{echo "<script language='javascript'>"; 
-         echo "alert(' i campi obbligatori devono essere inseriti');";
-        echo " </script>";
-         echo "<a href='inserisciOsservazione.php'>Torna alla pagina per inserire l'osservazione </a> ";
-       }
-       }
-       else{
+            } //chiudo for
+            echo "<script language='javascript'>"; 
+            echo "alert('Inserimento effettuato');";
+             echo " </script>";
+             echo "<a href='prova-sessioni.php'>Torna alla pagina personale </a> ";
+          
+        mysqli_close($conn); 
+       	 
+       	 
+       }//chiudo controllo accesso in post
+         else{
          echo "accesso errato alla pagina <a href='prova-sessioni.php'>Torna alla pagina personale </a> ";
             }
 
 
 
-	}
+	} // chiudo secondo if
     else
      {
         echo "<script language='javascript'>"; 
@@ -118,7 +142,7 @@ if(isset($_SESSION["autenticato"])&& isset($_SESSION["tipo"]))
           echo " </script>";
          header( "Refresh:0; url=prova-sessioni.php", true, 303);
      }
-}
+} //chiudo primo if
 else
 {
     echo "<script language='javascript'>"; 
