690bbd2f4a5e368a8202454e35dba6871bc6f22a
Karina Zinchenko <karina.19@hotmail.it>
2017-06-16 16:42:48 +0200

    Add files via upload

--- vistaSoci.php
+++ vistaSoci.php
@@ -16,23 +16,33 @@ if (isset($_SESSION["autenticato"]) && isset($_SESSION["tipo"])) {
 
         <div class="featured-heading">
             <h1>Soci dell'osservatorio</h1><br>
- <div class="row">
-                <div class="col-xs-12 col-sm-4 col-md-4 form-group">
-                 <form method="post" action="<?php echo $_SERVER['PHP_SELF'] ?>">
-                    <label>Selezionare il campo su cui eseguire la ricerca</label>
-                    <select class="soflow-color" name="scelta_ricerca">
-                        <option value="nome">Nome </option>
-                        <option value="cognome">Cognome </option>
-                        <option value="numero_socio">Numero Socio </option>
-                    </select>
-                </div> 
-
-                <div class="col-xs-6 col-sm-3 col-md-3 form-group">
-                    <label>Valore da cercare</label><input type="text" name="valore_ricerca" placeholder="Cosa vuoi cercare?">
-                </div> 
-                <input id="contact-submit" class="btn" type="submit" name="invio_ricerca" value="Cerca">
-                </form>
-</div>
+ 			<div id ="contact-info-ricerca" class="contact-info"> 
+            	<div id="panel-body-ricerca" class="panel-body">
+                 <form method="post" action="<?php echo $_SERVER['PHP_SELF'] ?>">  
+                 	<div class="col-xs-12 col-sm-2 col-md-2 form-group">                    
+                    </div>
+                    <div class="col-xs-12 col-sm-3 col-md-3 form-group">
+                        <label  id="label-ricerca">Valore da cercare</label>
+                        <input style="margin-bottom: 0; margin-right:0; min-height: 34px;" type="text" name="valore_ricerca" placeholder="Cosa vuoi cercare?">
+                    </div> 
+                 	<div class="col-xs-12 col-sm-3 col-md-3 form-group">
+                      <label id="label-ricerca">Campo di ricerca</label>
+                      <select style="margin-bottom: 0;" class="soflow-color" name="scelta_ricerca">
+                          <option value="nome">Nome </option>
+                          <option value="cognome">Cognome </option>
+                          <option value="numero_socio">Numero Socio </option>
+                      </select>
+                	</div>                     
+                    <div class="col-xs-12 col-sm-2 col-md-2 form-group" style="top:20px;">
+                		<input id="contact-submit" class="btn" type="submit" name="invio_ricerca" value="Cerca">
+                    </div> 
+                    <div class="col-xs-12 col-sm-2 col-md-2 form-group">                    
+                    </div>
+                </form>                
+                </div>
+			</div>
+           <br>
+           <br>
             <table class="table">
                 <thead class="thead-default">
                     <tr>
@@ -50,7 +60,7 @@ if (isset($_SESSION["autenticato"]) && isset($_SESSION["tipo"])) {
                    $valore=$_POST["valore_ricerca"];
                    $aux=$valore;
                    $valore=strtoupper(substr($valore, 0,1)).substr($aux,1);
-                   echo $valore;
+                   echo "<h3 style='color:#d3b483;'>Risultati per '".$valore."'</h3><br>";
 
                 $stmt = $conn->prepare("SELECT numero_socio, nome, cognome, data_nascita FROM anagrafica WHERE ".$condizione."='".$valore."';");
                 }
