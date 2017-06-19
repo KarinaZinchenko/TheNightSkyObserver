c088ef40817496ba63ea732074042b9bdac934d7
Karina Zinchenko <karina.19@hotmail.it>
2017-06-19 16:06:33 +0200

    Add files via upload

--- vistaCombinazioni.php
+++ vistaCombinazioni.php
@@ -1,6 +1,15 @@
+<head>
+    <title>Combinazioni telescopio - oculare</title>
+</head>
 <?php
 session_start();
 include("config.php");
+include("header.php");
+include("navbar.php");
+echo "<div class='container'>";
+echo "<div class='row-fluid'>";
+echo "<div class='span10 offset1'>";
+
 if (isset($_SESSION["autenticato"]) && isset($_SESSION["tipo"])) {
     # if ($_SESSION["tipo"] == "amministratore") {
         # if (!isset($_POST["invio"])) {
@@ -20,46 +29,49 @@ if (isset($_SESSION["autenticato"]) && isset($_SESSION["tipo"])) {
         $oculari[] = $row;
     }
     ?>
-    <html>
-    <head>
-      <title>Combinazioni telescopio - oculare</title>
-    </head>
-    <body>
-      <center>
-        <h1>Tabella combinazioni</h1><br><br>
-        <table>
-          <tr>
+    <div class="featured-heading">
+        <h1>Tabella combinazioni</h1><br>
+        <table class="table">
+            <thead class="thead-default">
+            <tr>
             <?php
-            echo "<th>Telescopio | Oculare</th>";
+            echo "<th style='color: #c47780; vertical-align: middle'>TELESCOPIO | OCULARE</th>";
             foreach ($oculari as $oculare) {
-                echo "<td>". $oculare['nome'] ."</td>";
+                echo "<th id='combinazioni'>". $oculare['nome'] ."</th>";
             }
             echo "</tr>";
+            echo "</thead>";
             foreach ($telescopi as $telescopio) {
                 echo "<tr>";
-                echo "<td>". $telescopio['nome'] ."</td>";
+                echo "<th id='combinazioni'>". $telescopio['nome'] ."</th>";
                 foreach ($oculari as $oculare) {
                     $ingrandimentiPossibili = $telescopio['lunghezza_focale'] / $oculare['lunghezza_focale'];
                     $pupillaUscita = $oculare['lunghezza_focale'] / $telescopio['focal_ratio'];
                     $campoReale = $oculare['campo_visione_apparente'] / $ingrandimentiPossibili;
                     echo "<td>";
-                    echo "Ingrandimenti possibili: ". round($ingrandimentiPossibili, 2) ."<br>";
-                    echo "Pupilla di uscita: ". round($pupillaUscita, 2) ."<br>";
-                    echo "Campo reale di visione: ". round($campoReale, 2);
+                    echo "Ingrandimenti possibili: <label id='comblabel' style='font-style: italic'>". round($ingrandimentiPossibili, 2) ."</label><br>";
+                    echo "Pupilla di uscita: <label id='comblabel' style='font-style: italic'>". round($pupillaUscita, 2) ."</label><br>";
+                    echo "Campo reale di visione: <label id='comblabel' style='font-style: italic'>". round($campoReale, 2)."</label>";
                     echo "</td>";
                 }
                 echo "</tr>";
             }
             ?>
-          </table>
-         </center>
-       </body>
-     </html>
+        </table>
+    </div>
+<?php
+}  else
+{?>
+    <script>
+        swal({title:"Oops...!",text:"Non sei autorizzato.",type:"error",showConfirmButton:false});
+    </script>
     <?php
-} else {
-    echo "<script language='javascript'>";
-    echo "alert('Non sei autorizzato ');";
-    echo "</script>";
-    header("Refresh:0; index.php", true, 303);
+    header("Refresh:2; index.php", true, 303);
+
 }
+echo "</div>";
+echo "</div>";
+echo "</div>";
+
+include("footer.php");
 ?>
